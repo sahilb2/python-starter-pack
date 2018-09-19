ecb2e1cb07794ae415d8c4821afd2323e1995b54
HoelzelJon <hoelzeljon@gmail.com>
2018-09-19 01:39:05 -0500

    added comments for players

--- MyBot.py
+++ MyBot.py
@@ -1,12 +1,17 @@
+# keep these three import statements
 import game_API
 import fileinput
 import json
+
+# your import statements here
 import random
 
-first_line = True
+first_line = True # do not remove
 
+# global variables can go here
 stances = ["Rock", "Paper", "Scissors"]
 
+# loop for each turn
 for line in fileinput.input():
     if first_line:
         game = game_API.Game(json.loads(line))
@@ -14,9 +19,9 @@ for line in fileinput.input():
         continue
     game.update(json.loads(line))
 
-    # YOUR CODE BELOW
+    # ----------YOUR CODE BELOW----------
+    # this code will be executed each turn of the game
 
-    #destination_node = game.get_adjacent_nodes(game.get_self().location)[0]
     paths = game.shortest_paths(game.get_self().location, 3)
     if len(paths) > 0 and len(paths[0]) > 0:
         destination_node = paths[0][0]
@@ -24,7 +29,5 @@ for line in fileinput.input():
         destination_node = 3
 
     chosen_stance = stances[random.randint(0, 2)]
-
-    # YOUR CODE ABOVE
 
     game.submit_decision(destination_node, chosen_stance)
