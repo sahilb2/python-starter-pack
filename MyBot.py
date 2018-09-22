368d4783492487d17d47474fba83adcb6e92cbe1
sahilb2 <sahilbhatt@hotmail.com>
2018-09-22 14:33:05 -0500

    simple bot

--- MyBot.py
+++ MyBot.py
@@ -11,6 +11,14 @@ first_line = True # DO NOT REMOVE
 # global variables or other functions can go here
 stances = ["Rock", "Paper", "Scissors"]
 
+nodes_map = {}
+nodes_map[0] = [1, 6, 10]
+nodes_map[1] = [3, 0]
+nodes_map[3] = [1]
+nodes_map[6] = [0]
+nodes_map[10] = [0, 16]
+nodes_map[16] = [10]
+
 def get_winning_stance(stance):
     if stance == "Rock":
         return "Paper"
@@ -18,6 +26,44 @@ def get_winning_stance(stance):
         return "Scissors"
     elif stance == "Scissors":
         return "Rock"
+
+def get_next_node(me, game):
+    if me.location == me.destination:
+        if game.has_monster(me.location) and (game.get_monster(me.location).respawn_counter < 0.2 * game.get_monster(me.location).respawn_rate):
+            return me.location
+        next_nodes = nodes_map[me.location]
+        if 0 in next_nodes:
+            if not game.get_monster(0).dead or (game.get_monster(0).respawn_counter < 0.3 * game.get_monster(0).respawn_rate):
+                return 0
+        for node in next_nodes:
+            if game.has_monster(node):
+                monster = game.get_monster(node)
+                if not monster.dead or (monster.respawn_counter < 0.2 * monster.respawn_rate):
+                    if me.location == 0:
+                        nodes_map[0] = next_nodes[::-1]
+                        if me.scissors < 3:
+                            temp = nodes_map[0][0]
+                            nodes_map[0][0] = nodes_map[0][1]
+                            nodes_map[0][1] = temp
+                    return node
+        if me.location == 1:
+            return 0
+        return next_nodes[0]
+    elif me.destination == -1:
+        return 0
+    else:
+        return me.destination
+
+def get_next_stance(me, game):
+    if game.has_monster(me.location):
+        monster = game.get_monster(me.location)
+        if not monster.dead:
+            return get_winning_stance(monster.stance)
+    opponent = game.get_opponent()
+    if opponent.stance == "Invalid Stance":
+        return "Paper"
+    return get_winning_stance(opponent.stance)
+
 
 # main player script logic
 # DO NOT CHANGE BELOW ----------------------------
@@ -33,25 +79,8 @@ for line in fileinput.input():
 
     me = game.get_self()
 
-    if me.location == me.destination: # check if we have moved this turn
-        # get all living monsters closest to me
-        monsters = game.nearest_monsters(me.location, 1)
-
-        # choose a monster to move to at random
-        monster_to_move_to = monsters[random.randint(0, len(monsters)-1)]
-
-        # get the set of shortest paths to that monster
-        paths = game.shortest_paths(me.location, monster_to_move_to.location)
-        destination_node = paths[random.randint(0, len(paths)-1)][0]
-    else:
-        destination_node = me.destination
-
-    if game.has_monster(me.location):
-        # if there's a monster at my location, choose the stance that damages that monster
-        chosen_stance = get_winning_stance(game.get_monster(me.location).stance)
-    else:
-        # otherwise, pick a random stance
-        chosen_stance = stances[random.randint(0, 2)]
+    destination_node = get_next_node(me, game)
+    chosen_stance = get_next_stance(me, game)
 
     # submit your decision for the turn (This function should be called exactly once per turn)
     game.submit_decision(destination_node, chosen_stance)
